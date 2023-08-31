db124d57198e43af5d747e94dc76fa92bb9c1859
segauser <kovalchuksiargey@gmail.com>
2023-08-31 17:15:50 +0300

    Templates and Static Files Django

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "horoscope"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = "blog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / 'templates' # for templates root
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -110,6 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static' # for global static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- blog/horoscope/views.py
+++ blog/horoscope/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
 zodiac_dict = {
-    "aries": "Овен - 1-й знак",
-    "taurus": "Телец - 2-й знак",
-    "gemini": "Близнецы - 3-й знак",
-    "cancer": "Рак - 4-й знак",
-    "leo": "Лев - 5-й знак",
-    "virgo": "Дева - 6-й знак",
-    "libra": "Весы - 7-й знак",
-    "scorpio": "Скорпион - 8-й знак",
-    "segittarius": "Стрелец - 9-й знак",
-    "capricorn": "Козерог - 10-й знак",
-    "aquarius": "Водолей - 11-й знак",
-    "pisces": "Рыбы - 12-й знак",
+    "aries": "Овен /Aries - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).",
+    "taurus": "Телец /Taurus - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).",
+    "gemini": "Близнецы /Gemini - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).",
+    "cancer": "Рак /Cancer - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).",
+    "leo": "Лев /Leo - пятый знак зодиака, солнце (с 23 июля по 21 августа).",
+    "virgo": "Дева /Virgo - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).",
+    "libra": "Весы /Libra - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).",
+    "scorpio": "Скорпион /Scorpio - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).",
+    "segittarius": "Стрелец /Sagittarius - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).",
+    "capricorn": "Козерог /Capricorn - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).",
+    "aquarius": "Водолей /Aquarius - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).",
+    "pisces": "Рыбы /Pisces - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).",
 }
 
 types_dict = {
-    'fire' : ['aries', 'leo', 'segittarius'],
-    'earth' : ['taurus', 'virgo', 'capricorn'],
-    'air' : ['gemini', 'libra', 'aquarius'],
-    'water' : ['cancer', 'scorpio', 'pisces']
+    'fire': ['aries', 'leo', 'segittarius'],
+    'earth': ['taurus', 'virgo', 'capricorn'],
+    'air': ['gemini', 'libra', 'aquarius'],
+    'water': ['cancer', 'scorpio', 'pisces']
 }
 
 # print(zodiac_dict.keys())
@@ -37,17 +38,13 @@ types_dict = {
 
 def index(request):
     zodiacs = list(zodiac_dict)
-    li_elements = ""
-    for sign in zodiacs:
-        redirect_path = reverse("horoscope-name", args=[sign])
-        li_elements += f'<li><a href="{redirect_path}">{sign.title()}</a></li>'
-    response = f"""
-    <ul>
-        {li_elements}
-    </ul>
-    """
-    return HttpResponse(response)
+    # li_elements += f'<li><a href="{redirect_path}">{sign.title()}</a></li>'
+    context = {
+        'zodiacs': zodiacs
+    }
+    return render(request, 'horoscope/index.html', context=context)
 
+# TODO - сделать представление для horoscope/type
 def type_info(request):
     types = list(types_dict)
     li_elements = ""
@@ -61,12 +58,12 @@ def type_info(request):
     """
     return HttpResponse(response)
 
-
+# [x]TODO - сделать представление для horoscope/type/element
 def element_info(request, type_element):
     elements = types_dict.get(type_element)
     li_elements = ""
     for element in elements:
-        redirect_path = reverse("type-elem-name", args=[element])
+        redirect_path = reverse("horoscope-name", args=[element])
         li_elements += f'<li><a href="{redirect_path}">{element.title()}</a></li>'
     response = f"""
     <ul>
@@ -77,13 +74,14 @@ def element_info(request, type_element):
 
 
 def get_info_sign_zodiac(request, sign_zodiac: str):
-    description = zodiac_dict.get(sign_zodiac, None)
-    if description:
-        return HttpResponse(f"<h2>{description}</h2>")
-    else:
-        return HttpResponseNotFound(f"Unknown znak zodiac - {sign_zodiac}")
+    description = zodiac_dict.get(sign_zodiac)
+    data = {
+        'description': description,
+        'sign': sign_zodiac,
+        'zodiacs': zodiac_dict
 
-
+    }
+    return render(request, 'horoscope/info_zodiac.html', context=data)
 
 
 def get_info_sign_zodiac_by_number(request, sign_zodiac: int):
